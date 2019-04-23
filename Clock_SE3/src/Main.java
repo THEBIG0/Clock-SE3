5039999d781236186d43aaaf75c019fcd13ca0ae
THEBIG0 <owendcgunter@gmail.com>
2019-04-23 16:47:00 +0930

    Set Time Clock
    -If you click increment button, time is set to current time
    -Have trouble trying to update digits in real time
    -first digit displays text on top of another and black background was not carried over even though it was copied and pasted.

--- Clock_SE3/src/Main.java
+++ Clock_SE3/src/Main.java
@@ -1,4 +1,7 @@
 
+import Clock.TestClockDigit;
+import java.util.Timer;
+
 /**
  *
  * @author
@@ -11,6 +14,10 @@ public class Main {
     public static void main(String[] args) {
         Clock.TestClockDigit test = new Clock.TestClockDigit();
         test.setVisible(true);
+        Clock.TestClockDigit.T timer = (new Clock.TestClockDigit()).new T();
+        Timer t = new Timer();
+        t.schedule(timer, 0,1000);
+  
     }
     
 }
